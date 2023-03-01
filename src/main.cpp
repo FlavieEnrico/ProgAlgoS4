8e691dbfc4e2208ecbb9d0caa1e905c306806e50
FlavieEnrico <flavie.enrico.74@gmail.com>
2023-03-01 15:08:25 +0100

    Triangles !

--- src/main.cpp
+++ src/main.cpp
@@ -19,20 +19,17 @@ int main(int argc, char* argv[])
     auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
     ctx.maximize_window();
     std::vector<std::vector<float>> coord;
-    // double coord[400];
+
     for (int i = 0; i < 100; i++)
     {
         std::vector<float> pos;
         pos.push_back(p6::random::number(-1, 1));
         pos.push_back(p6::random::number(-1, 1));
-        // coord[i]     = p6::random::number(-1, 1);
-        // coord[i + 1] = p6::random::number(-1, 1);
 
         std::vector<float> depl;
         depl.push_back(ceil(p6::random::number(-2, 1)));
         depl.push_back(ceil(p6::random::number(-2, 1)));
-        // coord[i + 2] = ceil(p6::random::number(-2, 1));
-        // coord[i + 3] = ceil(p6::random::number(-2, 1));
+
         while (depl[0] == 0 && depl[1] == 0)
         {
             depl[0] = ceil(p6::random::number(-2, 1));
@@ -52,17 +49,16 @@ int main(int argc, char* argv[])
         );
         for (int i = 0; i < 100; i++)
         {
-            ctx.square(
-                p6::TopLeftCorner(coord[i][0], coord[i][1]),
-                p6::Radius{0.1f}
+            ctx.equilateral_triangle(
+                p6::Center(coord[i][0], coord[i][1]),
+                p6::Radius{0.1f},
+                p6::Rotation{p6::Radians{0}}
             );
         }
         for (int i = 0; i < 100; i++)
         {
             coord[i][0] = coord[i][0] + 0.001f * coord[i + 1][0];
             coord[i][1] = coord[i][1] + 0.001f * coord[i + 1][1];
-            // coord[i]=coord[i]+0.001*coord[i+2];
-            // coord[i + 1] = coord[i + 1] + 0.001 * coord[i + 3];
         }
     };
 
