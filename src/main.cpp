0df9b977da1ddf00d5521a55ade73a8bc0802015
Maëlle Fleitz <maelle.fleitz@gmail.com>
2023-05-10 14:59:23 +0200

    Ajout 3D la sphère

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <stdlib.h>
+#include <glm/glm.hpp>
 #include <vector>
+#include "../src-common/glimac/common.hpp"
+#include "../src-common/glimac/sphere_vertices.hpp"
+#include "glm/gtc/type_ptr.hpp"
 #include "imgui.h"
 #include "p6/p6.h"
+
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "boid/boid.hpp"
 #include "doctest/doctest.h"
@@ -12,22 +17,89 @@ int main(int argc, char* argv[])
         if (doctest::Context{}.run() != 0)
             return EXIT_FAILURE;
         // The CI does not have a GPU so it cannot run the rest of the code.
-        const bool no_gpu_available = argc >= 2 && strcmp(argv[1], "-nogpu") == 0; // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        const bool no_gpu_available =
+            argc >= 2 && strcmp(argv[1], "-nogpu") == 0; // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
         if (no_gpu_available)
             return EXIT_SUCCESS;
     }
 
-    // Actual app
     auto ctx = p6::Context{{.title = "ProgAlgoS4"}};
     ctx.maximize_window();
+
+    int        width  = ctx.current_canvas_width();
+    int        height = ctx.current_canvas_height();
+    p6::Shader Shader =
+        p6::load_shader("shaders/Boid.vs.glsl", "shaders/Boid.fs.glsl");
+
+    // GLuint uMVPMatrix    = glGetUniformLocation(Shader.id(), "uMVPMatrix");
+    // GLuint uMVMatrix     = glGetUniformLocation(Shader.id(), "uMVMatrix");
+    // GLuint uNormalMatrix = glGetUniformLocation(Shader.id(), "uNormalMatrix");
+
+    glm::mat4 ProjMatrix = glm::perspective(
+        glm::radians(70.f),
+        (static_cast<float>(width) / static_cast<float>(height)), 0.1f, 100.f
+    );
+    glm::mat4 MVMatrix(1.0f);
+    MVMatrix               = glm::translate(MVMatrix, glm::vec3(0.f, 0.f, -5.f));
+    glm::mat4 NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+
+    // VBO
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    // std::vector<glimac::ShapeVertex> my_sphere;
+    // sphere
+    std::vector<glimac::ShapeVertex> my_sphere =
+        glimac::sphere_vertices(1.f, 32, 16);
+
+    // Remplir VBO
+    glBufferData(GL_ARRAY_BUFFER, my_sphere.size() * sizeof(glimac::ShapeVertex), my_sphere.data(), GL_STATIC_DRAW);
+
+    // VAO
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // activate vertex attributes
+    const GLuint VERTEX_ATTR_POSITION = 0;
+    glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+
+    const GLuint VERTEX_ATTR_NORMAL = 1;
+    glEnableVertexAttribArray(VERTEX_ATTR_NORMAL);
+
+    const GLuint VERTEX_ATTR_UV = 2;
+    glEnableVertexAttribArray(VERTEX_ATTR_UV);
+
+    // specification vertex attributes
+    glVertexAttribPointer(
+        VERTEX_ATTR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex),
+        (const GLvoid*)(offsetof(glimac::ShapeVertex, position))
+    );
+    glVertexAttribPointer(
+        VERTEX_ATTR_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex),
+        (const GLvoid*)(offsetof(glimac::ShapeVertex, normal))
+    );
+    glVertexAttribPointer(
+        VERTEX_ATTR_UV, 2, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex),
+        (const GLvoid*)(offsetof(glimac::ShapeVertex, texCoords))
+    );
+
+    // Debinding VBO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // Debinding VAO
+    glBindVertexArray(0);
+
+    // our boids
+    /*
     std::vector<Boid> flock;
-    float             size_boids       = 0.1;
+    float             size_boids       = 0.1f;
     float             separation_force = 2.0f;
 
     flock.resize(10);
-
+    */
     // Declare your infinite update loop.
     ctx.imgui = [&]() {
+        /*
         ImGui::Begin("Parameters");
         {
             int nb_boid = flock.size();
@@ -40,16 +112,44 @@ int main(int argc, char* argv[])
         }
         ImGui::End();
         ImGui::ShowDemoWindow();
+        */
     };
+
+    glEnable(GL_DEPTH_TEST);
     ctx.update = [&]() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Binding VAO
+        glBindVertexArray(vao);
+
+        // glUniformMatrix4fv(uMVMatrix, 1, GL_FALSE, glm::value_ptr(MVMatrix));
+
+        // glUniformMatrix4fv(uMVPMatrix, 1, GL_FALSE, glm::value_ptr(ProjMatrix *
+        // MVMatrix)); glUniformMatrix4fv(uNormalMatrix, 1, GL_FALSE,
+        // glm::value_ptr(NormalMatrix));
+
+        Shader.set("uMVMatrix", MVMatrix);
+        Shader.set("uMVPMatrix", ProjMatrix * MVMatrix);
+        Shader.set("uNormalMatrix", NormalMatrix);
+
+        glDrawArrays(GL_TRIANGLES, 0, my_sphere.size());
+        /*
         ctx.background(p6::NamedColor::ChartreuseWeb);
         for (auto& boid : flock)
         {
             boid.update_position(flock, size_boids, separation_force);
             Boid::draw(boid, ctx, size_boids);
         }
+        */
     };
-
+    glBindVertexArray(0);
     // Should be done last. It starts the infinite loop.
     ctx.start();
+
+    // Clear vbo & vao & texture
+    // glDeleteTextures(1, &my_texture);
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
+
+    return 0;
 }
